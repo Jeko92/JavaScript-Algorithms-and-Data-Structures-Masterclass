8597e5cf5eff016c7a94ac6b3575d6e0bba6f1b8
Jemal Khachidze <jemal.khachidze@fullhaus.de>
2023-11-29 11:13:23 +0100

    Section 3: Analyzing Performance of Arrays and Objects

---
+++ 3_Section-Analyzing-Performance-of-Arrays-and-Objects/arrays.js
@@ -0,0 +1,23 @@
+let names = ["Michael", "Melissa", "Andrea"];
+let values = [true, {}, [], 2, "awesome"];
+
+/*
+  Arrays are Ordered lists
+
+  Big O of Arrays:
+    -Insertion It depends... (when adding to end O(1), when adding to begining O(n))
+    -Removal It depends... (when removing from end O(1), when removing from begining O(n))
+    -Searching O(n)
+    -Access O(1)
+
+  Big O of Array Methods:
+    -Array.push O(1)
+    -Array.pop O(1)
+    -Array.shift O(n)
+    -Array.unshift O(n)
+    -Array.concat O(n)
+    -Array.slice O(n)
+    -Array.splice O(n)
+    -Array.sort O(n * log n)
+    -Array.forEach/map/filter/reduce O(n)
+  */
