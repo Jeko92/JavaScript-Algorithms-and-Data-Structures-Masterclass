d4bb62903fd56a7a6ef6ee1135a537f22632adcb
Jemal Khachidze <jemal.khachidze@fullhaus.de>
2023-11-29 11:13:23 +0100

    Section 3: Analyzing Performance of Arrays and Objects
    Signed-off-by: Jeko92 <jkhachidze92@gmail.com>

---
+++ 3_Section-Analyzing-Performance-of-Arrays-and-Objects/objects.js
@@ -0,0 +1,21 @@
+let instructor = {
+  firstName: "Kelly",
+  isInstructor: true,
+  favoruteNumbers: [1, 2, 3, 4],
+};
+
+/*
+Objects are Unordered Key, Value pairs
+
+Big O of Objects:
+  -Insertion O(1)
+  -Removal O(1)
+  -Searching O(n)
+  -Access O(1)
+
+Big O of Object Methods:
+  -Object.keys O(n)
+  -Object.values O(n)
+  -Object.entries O(n)
+  -hasOwnProperty O(1)
+*/
